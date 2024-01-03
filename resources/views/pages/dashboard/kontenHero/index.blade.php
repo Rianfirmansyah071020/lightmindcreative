76c2a140979b51ffc1f34f24b0e1dd990f4f6cd5
Rianfirmansyah071020 <rian071020@gmail.com>
2024-01-03 09:58:18 +0700

    install tinymce

--- resources/views/pages/dashboard/kontenHero/index.blade.php
+++ resources/views/pages/dashboard/kontenHero/index.blade.php
@@ -48,6 +48,43 @@
                         <td class="text-center">{{ $data['judul_teks_hero'] }}</td>
                         <td class="text-center">{{ $data['deskripsi_teks_hero'] }}</td>
                         <td class="text-center">
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                data-bs-target="#modalShow{{ $data['id_gambar_hero'] }}">
+                                <i class="fa-solid fa-eye"></i>
+                            </button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="modalShow{{ $data['id_gambar_hero'] }}" tabindex="-1"
+                                aria-labelledby="modalShowLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-lg">
+                                    <div class="modal-content">
+                                        <div class="modal-header bg-success text-white">
+
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="row d-flex justify-content-between">
+                                                <div class="col-lg-12">
+                                                    <img src="{{ $data['file_gambar_hero'] }}"
+                                                        alt="{{ $data['judul_teks_hero'] }}"
+                                                        style="all: initial; width:200px;">
+                                                </div>
+                                                <div class="col-lg-12">
+                                                    <h2 class="text-center">{{ $data['judul_teks_hero'] }}</h2>
+                                                    <p class="text-justify">{{ $data['deskripsi_teks_hero'] }}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-success"
+                                                data-bs-dismiss="modal">Close</button>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                             <a href="{{ route('hero.edit', [$data['id_gambar_hero'], $data['id_teks_hero']]) }}"
                                 class="btn btn-warning"><i class="fa-solid fa-pen-to-square"></i></a>
                             <a href="{{ route('hero.delete', [$data['id_gambar_hero'], $data['id_teks_hero']]) }}"
